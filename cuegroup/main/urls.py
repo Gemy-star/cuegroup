24838d6f93a9a1aeeaa253241cb6759999d4dd57
Gemy-star <m.yassen.93@gmail.com>
2021-08-30 21:24:03 +0200

    final dash

--- cuegroup/main/urls.py
+++ cuegroup/main/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home_page, name='home-page'),
     path('create/individual', views.add_individual_request, name='add-individual'),
     path('create/company', views.add_company_request, name='add-company'),
+    path('dashboard', views.use_dash, name='dashboard'),
+    path('logout', views.logoutUser, name='logout'),
+    path('all/individual', views.all_individual, name='all'),
 
 ]
--- cuegroup/main/views.py
+++ cuegroup/main/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import IndiviualRequest, CompanyRequest
+from .models import User
+from django.contrib.auth import authenticate, logout, login
 
 
 # Create your views here.
 def home_page(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return redirect('home-page')
     return render(request, 'main/home.html')
+
+
+def all_individual(request):
+    all = IndiviualRequest.objects.all()
+    return render(request, 'main/all_individual.html', context={"all": all})
+
+
+def use_dash(request):
+    context = {"user": User.objects.get(pk=request.user.pk)}
+    return render(request, 'main/user-dash.html', context=context)
 
 
 def add_individual_request(request):
@@ -15,13 +36,22 @@ def add_individual_request(request):
         phone = request.POST.get('phone')
         country = request.POST.get('country')
         dob = request.POST.get('dob')
+        password = request.POST.get('password')
+        user = User.objects.create_empuser(email=email, first_name=first_name, last_name=last_name, password=password,
+                                           address=country, phone=phone)
         individual = IndiviualRequest(first_name=first_name, last_name=last_name, phone=phone, country=country, DOB=dob,
                                       email=email)
         individual.save()
-        return redirect('home-page')
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
     else:
         return render(request, 'main/indiviual.html', )
 
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home-page')
 
 
 def add_company_request(request):
